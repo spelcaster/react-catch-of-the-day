b2078daba54beb56271322149a574f9e175c5b11
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2017-02-08 19:08:24 -0200

    adding dependency and firebase connection
    - Added re-base to handle firebase connection;
    - Added a default connection file to a firebase database, you must fill
      it with your firebase info;
    - Added base.js to .gitignore;

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: '<FIREBASE_API_KEY>',
+  authDomain: '<FIREBASE_AUTH_DOMAIN>',
+  databaseURL: '<FIREBASE_DATABASE_URL>'
+})
+
+export default base
